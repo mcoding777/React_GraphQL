b6cb470d4476ddfd4e35401cc054bb2967453116
misun <mcoding777@naver.com>
2022-07-08 08:34:50 +0900

    feat: useQuery 사용

--- src/routes/Movies.tsx
+++ src/routes/Movies.tsx
@@ -1,3 +1,27 @@
+import { gql, useQuery } from "@apollo/client";
+
+const ALL_MOVIES = gql`
+  {
+    allMovies {
+      title
+    }
+  }
+`;
+
 export const Movies = () => {
-    return <div>This is a list of movies</div>
+  const { loading, error, data } = useQuery(ALL_MOVIES);
+
+  // 로딩중이면 로딩중 화면
+  if (loading) return <h1>Loading...</h1>;
+
+  // 에러 뜨면 에러중 화면
+  if (error) return <h1>Could not fetch ㅠㅠ</h1>;
+
+  return (
+    <div>
+      {data.allMovies.map((item: any) => (
+        <li key={item.id}>{item.title}</li>
+      ))}
+    </div>
+  );
 };
