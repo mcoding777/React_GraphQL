abbae8aeb1ea6d0265e3ab7df06b92ffd2b25fae
misun <mcoding777@naver.com>
2022-07-08 09:51:29 +0900

    feat: Movie 화면 스타일링

--- src/routes/Movie.tsx
+++ src/routes/Movie.tsx
@@ -6,6 +6,8 @@ const GET_MOVIE = gql`
     movie(id: $movieId) {
       id
       title
+      medium_cover_image
+      rating
     }
   }
 `;
@@ -18,9 +20,15 @@ export const Movie = () => {
     },
   });
 
-  console.log(data);
-
-  if (loading) return <h1>Fetching movie...</h1>;
-
-  return <div>{data.movie.title}</div>;
+  return (
+    <div className="flex h-[100vh] w-full items-center justify-around bg-gradient-to-r from-[#d754ab] to-[#fd723a] text-white">
+      <div className="ml-[10px] w-[50%]">
+        <h1 className="text-[65px] mb-[15px]">{loading ? "Loading..." : data?.movie?.title}</h1>
+        <h4 className="text-[35px] mb-[10px]">⭐ {data?.movie?.rating}</h4>
+      </div>
+      <div className="w-[25%] h-[60%] bg-transparent rounded-md">
+        <img src={data?.movie?.medium_cover_image} alt="영화 포스터" width="100%" height="100%" />
+      </div>
+    </div>
+  );
 };
